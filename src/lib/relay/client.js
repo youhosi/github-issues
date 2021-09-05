23366adf769e4d07affc8cffa503d97f61ee9366
youhosi <sebastiansycz99@icloud.com>
2021-09-05 16:43:51 +0200

    Update relay client store. 🎊

--- src/lib/relay/client.js
+++ src/lib/relay/client.js
@@ -6,22 +6,27 @@ import nextConfig from "next/config";
 import fetchRelay from "./fetchRelay";
 
 const { __RELAY_TTL__ } = nextConfig().publicRuntimeConfig;
+const storeConfig = { gcReleaseBufferSize: 8, queryCacheExpirationTime: __RELAY_TTL__ };
 
 let environment = null;
 
 export const relayClientEnvironment = (relay) => {
-    if (environment) return environment;
+    if (!environment) {
+        const source = new RecordSource(relay);
+        const network = Network.create(fetchRelay);
+        const store = new Store(source, storeConfig);
 
-    const source = new RecordSource(relay);
-    const store = new Store(source, { gcReleaseBufferSize: 10, queryCacheExpirationTime: __RELAY_TTL__ });
-    const network = Network.create(fetchRelay);
+        environment = new Environment({
+            configName: "Relay Client",
+            isServer: false,
+            network,
+            store,
+        });
+    }
 
-    environment = new Environment({
-        configName: "Relay Client",
-        isServer: false,
-        network,
-        store,
-    });
+    if (relay && typeof relay === "object") {
+        environment.getStore().publish(new RecordSource(relay));
+    }
 
     return environment;
 };

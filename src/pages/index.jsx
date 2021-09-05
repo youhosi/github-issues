87ca7f40b14f5979e3dd3ec4154938b49acb18e9
youhosi <sebastiansycz99@icloud.com>
2021-09-05 21:13:54 +0200

    Move graphql tag to relay hook, and import graphql file in relaySSR.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 
 import Repositories from "components/Repositories";
-import { graphql, useLazyLoadQuery, relaySSR, fetchQuery } from "lib/relay";
-
-const indexQuery = graphql`
-    query pages_indexQuery {
-        viewer {
-            ...RepositoriesFragment
-        }
-    }
-`;
+import { useLazyLoadQuery, graphql, relaySSR, fetchQuery } from "lib/relay";
 
 const IndexPage = () => {
-    const data = useLazyLoadQuery(indexQuery, null, { fetchPolicy: "store-or-network" });
+    const data = useLazyLoadQuery(
+        graphql`
+            query pages_indexQuery {
+                viewer {
+                    ...RepositoriesFragment
+                }
+            }
+        `,
+        null,
+        { fetchPolicy: "store-or-network" }
+    );
 
     return (
         <div className="container">
             <div className="repositories">
-
-
                 <h2>Repositories</h2>
 
                 <Repositories viewer={data.viewer} />
@@ -27,8 +27,6 @@ const IndexPage = () => {
             <div className="recent-issues">
                 <h2>Recent Issues</h2>
             </div>
-
-
 
             <style jsx>{`
                 .container {
@@ -43,8 +41,8 @@ const IndexPage = () => {
     );
 };
 
-export const getServerSideProps = relaySSR(async (context) => {
-    const { environment } = context;
+export const getServerSideProps = relaySSR(async ({ environment }) => {
+    const indexQuery = await import("generated/relay/pages_indexQuery.graphql");
 
     await fetchQuery(environment, indexQuery, null, { fetchPolicy: "network-only" }).toPromise();
 });
